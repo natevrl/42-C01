2e44702161877d56afd2d1d39809480d77c9fcdb
Nathan BENHADOUCHE <nbenhado@e3r2p6.42.fr>
2021-07-02 11:08:36 +0200

    norminette 1

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -6,34 +6,35 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 18:20:55 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/07/02 11:01:38 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/07/02 11:07:50 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void    ft_swap(int *a, int *b)
+void	ft_swap(int *a, int *b)
 {
-    int    c;
+	int	c;
 
-    c = *b;
-    *b = *a;
-    *a = c;
+	c = *b;
+	*b = *a;
+	*a = c;
 }
 
-void ft_rev_int_tab(int *tab, int size)
+void	ft_rev_int_tab(int	*tab, int	size)
 {
+	int	a;
 
-    int a = 0;
-    while((size != a + 1) && (size != a))
-    {
-        ft_swap(&tab[a], &tab[size - 1]);
-        size--;
-        a++;
-    }
+	a = 0;
+	while (size != a + 1 && size != a)
+	{
+		ft_swap(&tab[a], &tab[size - 1]);
+		size--;
+		a++;
+	}
 }
 
-int main()
+int	main(void)
 {
     int suite[9] = {1, 5, 2, 3, 4, 8, 8, 8, 7};
     ft_rev_int_tab(suite, 9);
