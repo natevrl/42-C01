aa6085a1b441085568dac40c2634c92b9f7384a1
Nathan BENHADOUCHE <nbenhado@e3r2p6.42.fr>
2021-07-02 11:02:38 +0200

    exo 7 et 8 reussis

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -6,28 +6,38 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 18:20:55 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/07/01 21:40:37 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/07/02 11:01:38 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdio.h>
+
+void    ft_swap(int *a, int *b)
+{
+    int    c;
+
+    c = *b;
+    *b = *a;
+    *a = c;
+}
 
 void ft_rev_int_tab(int *tab, int size)
 {
 
-	while(size >  0)
-	{
-		int a = tab[size-1];
-		a = a + 48;
-		write(1, &a, 1);
-		size--;
-	}
+    int a = 0;
+    while((size != a + 1) && (size != a))
+    {
+        ft_swap(&tab[a], &tab[size - 1]);
+        size--;
+        a++;
+    }
 }
 
 int main()
 {
-	int i = 4;
-	int suite[5] = {1 , 5, 2, 3, 4};
-	ft_rev_int_tab(suite, 5);
-	return (0);
+    int suite[9] = {1, 5, 2, 3, 4, 8, 8, 8, 7};
+    ft_rev_int_tab(suite, 9);
+    for(int i = 0; i < 9; i++)
+    printf(" %d, ", suite[i]);
+    return (0);
 }
--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -6,12 +6,11 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 20:51:32 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/07/01 22:15:08 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/07/02 10:20:21 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-
 
 void	ft_swap(int *a, int *b)
 {
@@ -22,19 +21,19 @@ void	ft_swap(int *a, int *b)
 	*a = c;
 }
 
-void ft_sort_int_tab(int *tab, int size)
+void	ft_sort_int_tab(int	*tab, int	size)
 {
-	int a = 0;
-	int b = 0;
+	int	a;
+	int	b;
+
+	a = 0;
 	while (a < size)
 	{
 		b = 0;
-		while(b < size)
+		while (b < size)
 		{
-			if (tab[a] > tab[b])
-			{
+			if (tab[a] < tab[b])
 				ft_swap(&tab[a], &tab[b]);
-			}
 			b++;
 		}
 		a++;
@@ -43,9 +42,9 @@ void ft_sort_int_tab(int *tab, int size)
 
 int main()
 {
-	int suite[4] = {5, 8, 2, 12};
-	ft_sort_int_tab(suite, 4);
-	for (int i = 0 ; i < 4; i++)
+	int suite[7] = {26, 84, 12, 142, 4, 75, 100};
+	ft_sort_int_tab(suite, 7);
+	for (int i = 0 ; i < 7; i++)
     	printf(" %d, ", suite[i]);
 	return (0);
 }
